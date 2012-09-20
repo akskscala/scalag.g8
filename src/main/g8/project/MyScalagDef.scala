5a777ccdce60555d3b58d4ea73f59c59e5c2eaa4
Kazuhiro Sera <seratch@gmail.com>
2012-09-20 20:20:34 +0900

    Added echo example

--- src/main/g8/project/MyScalagDef.scala
+++ src/main/g8/project/MyScalagDef.scala
@@ -6,5 +6,14 @@ object MyScalagDef extends Plugin {
 
   ScalagPlugin.addCommands(builtin.all:_*)
 
+  ScalagPlugin.addCommand(
+    namespace = "echo",
+    args = Seq("path", "content"),
+    description = "Writes the content",
+    operation = { case ScalagInput(path :: content :: Nil, settings) =>
+      FilePath(path).writeIfNotExists(content, "UTF-8")
+    }
+  )
+
 }
 
